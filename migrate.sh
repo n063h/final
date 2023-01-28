c3e45e580e0997e0f84a4bcfd6de06cf55eea238
hang lv <xlv20@fudan.edu.cn>
2023-01-28 18:21:14 +0800

    1

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,8 @@
-scp -P $port  root@region-11.autodl.com:/etc/hosts /etc
 scp -P $port -r  root@region-11.autodl.com:/root/autodl-tmp/datasets /root/autodl-tmp
 mkdir /root/autodl-tmp/checkpoints
 mkdir /root/tf-logs
+
+cat << EOF >> /etc/hosts
+151.101.1.194 github.global.ssl.fastly.net
+140.82.114.3 github.com
+EOF
